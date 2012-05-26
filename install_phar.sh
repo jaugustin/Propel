405b4223be528c36261d7ac94b6a2c27c598b083
sanemat <o.gata.ken@gmail.com>
2012-05-26 15:09:44 +0900

    Add install phar script

---
+++ install_phar.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+php -r "echo version_compare(PHP_VERSION, '5.3.0', '<') ? 'TRUE' : 'FALSE';"
+less_than_php_5_3=$?
+if [ ${less_than_php_5_3} = 'TRUE' ]
+then
+  wget http://pecl.php.net/get/phar-2.0.0.tgz
+  tar -xzf phar-2.0.0.tgz
+  sh -c "cd phar-2.0.0 && phpize && ./configure && make && sudo make install"
+  echo "extension=phar.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+fi
